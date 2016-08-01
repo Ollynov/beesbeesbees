6238347bb701afaad1eaf5f8e20519dfef5e1167
Ollynov <ollynov@gmail.com>
2016-08-01 15:22:56 -0700

    Finished HoneyMakerBee with both property inheritance and new methods defined

--- src/HoneyMakerBee.js
+++ src/HoneyMakerBee.js
@@ -1,8 +1,15 @@
 var HoneyMakerBee = function() {
-	this.age = 10;
+	Bee.call(this);
+  this.age = 10;
 	this.job = "make honey";
 	this.honeyPot = 0;
+
+  
 };
+
+HoneyMakerBee.prototype = Object.create(Bee.prototype);
+HoneyMakerBee.prototype.constructor = HoneyMakerBee;
+
 
 HoneyMakerBee.prototype.makeHoney = function() {
 	this.honeyPot++;
